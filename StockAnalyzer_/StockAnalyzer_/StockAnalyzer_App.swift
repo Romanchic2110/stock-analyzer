f95208174caa54e439ad47ef90f792d9cc33505e
Куликов Роман <romankulikov211@gmail.com>
2026-03-26 19:56:06 +0300

    Редактирование

---
+++ StockAnalyzer_/StockAnalyzer_/StockAnalyzer_App.swift
@@ -0,0 +1,17 @@
+//
+//  StockAnalyzer_App.swift
+//  StockAnalyzer_
+//
+//  Created by Roman on 26.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct StockAnalyzer_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
